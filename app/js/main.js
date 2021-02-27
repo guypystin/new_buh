776437333474cad5542bdd5bb03bc9849e7dd6de
guypystin <51515987+guypystin@users.noreply.github.com>
2021-02-27 23:18:55 +0300

    вторая карусель

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,14 @@
 $(function() {
     $('.first__slider').slick({
         dots:true,
+        autoplay: true,
+        autoplaySpeed: 2000,
     });
-
+    $('.four__slider').slick({
+        dots:true,
+        slidesToShow : 4,
+        lidesToScroll : 4,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 })
- 
